97ffc8bd6cbaef96d34a09e95f4b5a064ed32963
Pierre Segonne <pierre.segonne@tmrow.com>
2022-02-26 15:04:08 +0100

    Use gunicorn for cloud run

---
+++ make_predict_cloud-run.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+# POST method predict
+curl -d '{
+    "age":{
+      "0": 0.038076
+    },
+    "sex":{
+      "0": 0.050680
+    },
+    "bmi":{
+      "0": 0.061696
+    },
+    "bp":{
+       "0": 0.021872
+    },
+    "s1":{
+       "0": -0.044223
+    },
+    "s2":{
+       "0": -0.034821
+    },
+    "s3":{
+       "0": -0.043401
+    },
+    "s4":{
+       "0": -0.002592
+    },
+    "s5":{
+       "0": 0.019908
+    },
+    "s6":{
+       "0": -0.017646
+    }
+}'\
+     -H "Content-Type: application/json" \
+     -H "Authorization: Bearer $(gcloud auth print-identity-token)" \
+     -X POST https://mlops-pipeline-jfnx5klx2a-lz.a.run.app/predict
